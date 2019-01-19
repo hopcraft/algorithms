5e1256828fc2ed262600757bc442493f96dd18c0
hopcraft <hopcraft@foxmail.com>
2019-01-19 15:16:37 +0800

    accomplish quicksort program

--- c/quicksort.c
+++ c/quicksort.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void quickSort(int v[]);
-void swap(int v[], i, j);
+void quickSort(int v[], int n);
+void swap(int v[], int i, int j);
+int scmp(const void *p1, const void *p2);
 
 int main() {
   int v[] = {
@@ -10,19 +12,36 @@ int main() {
     376, 990, 99, 78, 334, 343,
     376, 990, 99, 78, 334, 343
   };
-  quickSort(v);
+  int sizeofAll = (int)sizeof(v);
+  int sizeofOne = (int)sizeof(v[0]);
+  int n = sizeofAll / sizeofOne;
+  printf("sizeAll = %d, sizeOne = %d, n = %d\n", sizeofAll, sizeofOne, n);
+  quickSort(v, n);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", v[i]);
+  }
+  printf("\n");
   return 0;
 }
 
-void quickSort(int v[]) {
-  int n = sizeof(v) / sizeof(v[0]);
+void quickSort(int v[], int n) {
+  int i, last;
   if (n <= 1) {
     return;
   }
-  // @TODO 待实现核心逻辑
+  swap(v, 0, rand() % n);
+  last = 0;
+  for (i = 1; i < n; i++) {
+    if (v[i] < v[0]) {
+      swap(v, ++last, i);
+    }
+  }
+  swap(v, 0, last);
+  quickSort(v, last);
+  quickSort(v+last+1, n-last-1);
 }
 
-void swap(int v[], i, j) {
+void swap(int v[], int i, int j) {
   int temp = v[i];
   v[i] = v[j];
   v[j] = temp;
